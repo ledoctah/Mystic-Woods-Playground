5b8b4585e80f8bb82c8e4c549d88517058762859
Matheus Ferreira <matheusferr2015@gmail.com>
2022-07-25 19:21:55 -0300

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+  Vector2 movementInput;
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+  }
+
+  void OnMove(InputValue movementValue)
+  {
+    
+  }
+}
