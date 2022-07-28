841c519e76d016c44f44fde78438c66445871813
Matheus Ferreira <matheusferr2015@gmail.com>
2022-07-27 21:38:46 -0300

    feat: add slime health bar

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
   Animator animator;
+  float healthBarInitialSize;
+  float initialHealth;
+  long lastHideTimestamp;
 
+  public SpriteRenderer healthBar;
   public float health = 1;
   public float Health
   {
@@ -30,17 +34,42 @@ public class EnemyController : MonoBehaviour
   public void Start()
   {
     animator = GetComponent<Animator>();
+
+    initialHealth = Health;
+
+    healthBarInitialSize = healthBar.transform.localScale.x;
   }
 
 
   public void Defeat()
   {
+    healthBar.enabled = false;
     animator.SetTrigger("Defeated");
   }
 
   public void TakeDamage()
   {
+    healthBar.enabled = true;
+
     animator.SetTrigger("Damaged");
+
+    float percentage = Health / initialHealth;
+
+    healthBar.transform.localScale = new Vector2(percentage * healthBarInitialSize, healthBar.transform.localScale.y);
+
+    long time = System.DateTime.Now.ToFileTimeUtc();
+
+    lastHideTimestamp = time;
+
+    StartCoroutine(HideHealthBar(time));
+  }
+
+  private IEnumerator HideHealthBar(long time) {
+    yield return new WaitForSeconds(2);
+    
+    if(time == lastHideTimestamp) {
+      healthBar.enabled = false;
+    }
   }
 
   public void RemoveEnemy() {
